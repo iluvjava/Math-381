e127ec5cab4ed1cce589f8ab2b6f610b361e8f2e
A.L <32211153+iluvjava@users.noreply.github.com>
2020-01-13 21:25:27 -0800

    This is the new shit

---
+++ lp_generate.py
@@ -0,0 +1,14 @@
+"""
+Name: Hongda Li
+Class: Math 381
+This file is dedicated to creating LP solve text for the Lpsolve IDE.
+
+The codes needs python 3.6 or higher.
+"""
+
+class LPGen:
+    """
+        The sub for defining the basic operations of generating the text for LP problems. 
+    """
+    def __init__(self):
+        pass
