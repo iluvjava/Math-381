67e34d745ba50824f5d5aadfc2be4fac92b6850d
Alto Legato <victor1301166040@gmail.com>
2020-02-02 01:17:29 -0800

    This is the new shit

---
+++ Project1/main.py
@@ -0,0 +1,13 @@
+"""
+Name: Group 4
+Class: Winter 2020 math 381:
+
+This is a file that is used to generate the Lpsolve text input for the problem.
+
+"""
+
+
+
+
+if __name__ == "__name__":
+    pass
